423fb2fc166d64671c972185b0e854a4e7a1bf30
Eduardo Hurtado <eduhurtado.lopez@gmail.com>
2025-06-22 11:58:35 +0200

    Update jwt_auth_users.py

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -17,7 +17,7 @@ SECRET = "5giwefr7234oit674931207fhh98fg32"
 
 router = APIRouter(prefix="/jwtauth", 
                    tags=["jwtauth"],
-                   responses={status.HTTP_404_NOT_FOUND: {"message":"No encontrado"}})
+                   responses={status.HTTP_404_NOT_FOUND: {"message":"Not found"}})
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 # Start local server: uvicorn jwt_auth_users:app --reload
@@ -68,7 +68,7 @@ def search_user(username:str):
 # Token validation process
 async def auth_user(token: str = Depends(oauth2)):
 
-    exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticación inválidas", headers={"WWW-Authenticate": "Bearer"})
+    exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials", headers={"WWW-Authenticate": "Bearer"})
     try: 
         username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
         if username is None:
@@ -80,20 +80,20 @@ async def auth_user(token: str = Depends(oauth2)):
 
 async def current_user(user: User = Depends(auth_user)):
     if user.disabled: 
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticación inválidas", headers={"WWW-Authenticate": "Bearer"})
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials", headers={"WWW-Authenticate": "Bearer"})
     return user
 
 @router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User is not correct")
     
     user = search_user_db(form.username)
 
     # Comparison of login password (normal) and database password (encrypted) 
     if not crypt.verify(form.password, user.password):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password is not correct")
     
     # Secure authentication with a pre-defined duration limit
     # This access_token is un JSON because the method is JWT. Then I encrypt it. 
